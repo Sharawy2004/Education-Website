50d660d30bf4d95295a794a59028c97052a80b50
Mohamed <sharawym117@gmail.com>
2025-11-18 01:20:20 +0200

    push githup

---
+++ Education Website/assets/Js/main.js
@@ -0,0 +1,32 @@
+/*=============== SHOW MENU ===============*/
+let open_menu = document.getElementById("open_menu");
+let list = document.querySelector(".list");
+let close_menu = document.getElementById("close_menu");
+
+open_menu.addEventListener("click" , ()=>{
+    list.classList.add("show")
+})
+
+close_menu.addEventListener("click" , ()=>{
+    list.classList.remove("show")
+})
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    let header = document.getElementById("header");
+
+    if(this.scrollY >= 50){
+        header.classList.add("scroll_header")
+    }else{
+        header.classList.remove("scroll_header")
+    }
+}
+
+window.addEventListener(`scroll` , scrollHeader)
